2535d84f3ef58c1ba240c3584ff829f5af0b6d6b
TruongDX8 <truonghusk17@gmail.com>
2020-03-25 20:31:25 +0700

    Add button+ Image

--- src/main/java/com/truongdx8/ss1/MainActivity.java
+++ src/main/java/com/truongdx8/ss1/MainActivity.java
@@ -3,12 +3,29 @@ package com.truongdx8.ss1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button buttontLogin;
+    TextView tvTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
+
+        buttontLogin = findViewById(R.id.btLogin);
+        tvTitle = findViewById(R.id.idTitle);
+
+        //Bat su kien click button
+        buttontLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("MainActivity","Nhan vao nut login");
+                tvTitle.setText("Xin chao");
+            }
+        });
     }
 }
