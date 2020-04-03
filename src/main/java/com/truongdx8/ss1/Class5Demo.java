c9af5ad6daf5d0bd524e4e6b30f1c92718d472f0
TruongDX8 <truonghusk17@gmail.com>
2020-04-03 18:12:29 +0700

    B5:

---
+++ src/main/java/com/truongdx8/ss1/Class5Demo.java
@@ -0,0 +1,22 @@
+package com.truongdx8.ss1;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class Class5Demo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class5_demo);
+
+    }
+
+}
